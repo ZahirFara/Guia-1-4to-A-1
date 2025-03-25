3f6a7711fec3b3866691b0257735bbc1e038eaee
ZahirFara <zahir@maserati.com.ar>
2025-03-25 12:14:15 -0300

    trabajo terminado 1.2.3

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    [SerializeField] int edadUsuario;
+    [SerializeField] float estaturaUsuario;
+    [SerializeField] bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Tengo " + edadUsuario + " años.");
+        Debug.Log("Mido " + estaturaUsuario + "metros.");
+        Debug.Log("Y es " + esDonante + " que soy donante de organos.");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
